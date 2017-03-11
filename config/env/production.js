ae970aace4fd36c7e4f49b1fbcfa1702301b8bc4
dyadyaJora <ja_massa97@mail.ru>
2017-03-11 19:16:41 +0300

    change host

--- config/env/production.js
+++ config/env/production.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   port: process.env.PORT || 8443,
   // Binding to 127.0.0.1 is safer in production.
-  host: process.env.HOST || 'glacial-shelf-86467.herokuapp.com' || 'localhost',
+  host: process.env.HOST,
   db: {
     uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
     options: {
